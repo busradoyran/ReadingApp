260dd1dafb9f9133af262edeea5e93e89a79d608
U200709045 <busradoyran@posta.mu.edu.tr>
2023-01-02 19:27:56 +0300

    final

--- app/src/main/java/com/yasemintoraman/readingapp/MainActivity.java
+++ app/src/main/java/com/yasemintoraman/readingapp/MainActivity.java
@@ -1,21 +1,80 @@
 package com.yasemintoraman.readingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editEmail, editPassword;
+    private String txtEmail, txtSifre;
+    private FirebaseAuth mAuth;
+
+    public MainActivity() {
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editEmail = (EditText)findViewById(R.id.sign_up_email);
+        editPassword = (EditText)findViewById(R.id.sign_up_password);
+
+        mAuth = FirebaseAuth.getInstance();
     }
-    public void toDoReadScreen(View view){
-        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-        startActivity(intent);
+
+    public void kayitOl(View view){
+        Thread loginScreen = new Thread() {
+            public void run() {
+                try {
+                    sleep(6000);
+                    //6 saniye sonunda class davet et.
+                    startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    finish();
+                }
+            }
+        };
+        loginScreen.start();
+
+        // Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        // startActivity(intent);
+
+        txtEmail = editEmail.getText().toString();
+        txtSifre = editPassword.getText().toString();
+
+        if(!TextUtils.isEmpty(txtEmail) && !TextUtils.isEmpty(txtSifre)){
+            mAuth.createUserWithEmailAndPassword(txtEmail,txtSifre)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(MainActivity.this, "Kayıt İşlemi Başarılı",Toast.LENGTH_SHORT).show();
+                            } else
+                                Toast.makeText(MainActivity.this, task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }else
+            Toast.makeText(this,"Email ve Şifre Boş Olamaz", Toast.LENGTH_SHORT).show();
 
     }
 
