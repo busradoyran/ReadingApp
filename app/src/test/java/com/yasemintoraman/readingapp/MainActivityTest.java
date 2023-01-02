bd7f3382a85e395ab1d675c7ec81faea3feeab32
U200709045 <busradoyran@posta.mu.edu.tr>
2023-01-02 21:00:16 +0300

    final

---
+++ app/src/test/java/com/yasemintoraman/readingapp/MainActivityTest.java
@@ -0,0 +1,13 @@
+package com.yasemintoraman.readingapp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainActivityTest {
+    @Test
+    public void isMultiplicationCorrect() {
+        assertEquals(4, 2 * 2);
+    }
+
+}
